926f73a74914de06c7e13d9e326d1a1d2c61585d
BiFT <biftyou@gmail.com>
2023-03-01 10:48:47 +0200

    added Item storage with needed custom items

---
+++ src/main/java/net/ocejlot/jakashuinya/jakashuinya/util/ItemStorage.kt
@@ -0,0 +1,23 @@
+package net.ocejlot.jakashuinya.jakashuinya.util
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemStack
+
+object ItemStorage {
+    val slimePlatformItem = ItemStack(Material.SLIME_BALL).apply {
+        itemMeta = itemMeta.also { its ->
+            its.displayName(MiniMessage.miniMessage().deserialize("<green>Слайм-платформа"))
+        }
+    }
+
+    val shears = ItemStack(Material.SHEARS).apply {
+        itemMeta = itemMeta.also { its ->
+            its.displayName(MiniMessage.miniMessage().deserialize("<purple>Чарівні ножиці"))
+            its.isUnbreakable = true
+            its.addEnchant(Enchantment.DIG_SPEED, 10, true)
+        }
+    }
+}
